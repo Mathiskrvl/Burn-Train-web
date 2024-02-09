b42b4d2cb478e778fc9154b4622d1e1f1f4399b9
Mathis KERAVAL <mkeraval@mediametrie.fr>
2024-02-10 00:01:46 +0100

    rebase the vite config for github page

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  base: '/Burn-Train-web/',
   plugins: [
     topLevelAwait()
   ]
